bbfcc7a521fe5790d2d0436c17b4d85acb70b522
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-10-22 04:58:37 +0500

    Aqvo proectni boshladik

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useTransition } from 'react'
+import { useTranslation } from "react-i18next";
+const Navbar = () => {
+    const {t, i18n} = useTransition();
+  return (
+    <div>
+           
+    </div>
+  )
+}
+
+export default Navbar
--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,21 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import uzTranslation from '../public/Locals/en.json';
+import enTranslation from "../public/Locals/ru.json";
+import ruTranslation from "../public/Locals/uz.json";
 
 i18n
-
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources: {
+      uz: { translation: uzTranslation },
+      en: { translation: enTranslation },
+      ru: { translation: ruTranslation },
+    },
     fallbackLng: 'en',
-    debug: true,
+    debug: false,
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
     interpolation: {
-      escapeValue: false, 
-    }
+      escapeValue: false,
+    },
   });
-
 
 export default i18n;
