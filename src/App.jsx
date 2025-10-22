67a1f20efb819632c1a9e8ac856b1ac29ec45330
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-10-23 00:10:29 +0500

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./App.css"
+import Navbar from './Components/Navbar/Navbar'
 const App = () => {
   return (
     <div>
-        
+         <Navbar/>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,12 @@ import { useTranslation } from "react-i18next";
 const Navbar = () => {
     const {t, i18n} = useTransition();
   return (
-    <div>
-           
+    <div className='navbar'>
+          <div className='container'>
+                <div className="navbar__container">
+                      
+                </div>
+          </div> 
     </div>
   )
 }
